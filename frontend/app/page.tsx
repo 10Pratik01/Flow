8bcbd9aaaa6adcc48a0d90c09b30cfd4a257046a
10Pratik01 <using0this@gmail.com>
2025-12-07 00:43:34 +0530

    initialization complete, navbar completed and started sidebar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
+import DashboardWrapper from "./dashboardWrapper";
+
 export default function Home() { 
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to the home page</h1>
     </div>
   )
 }
