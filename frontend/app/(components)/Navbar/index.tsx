4cbaf9384478f5179217215e0d69ef7a687028cd
10Pratik01 <using0this@gmail.com>
2025-12-08 21:07:16 +0530

    Added Images for the project, udated settings global.css
    updated sidebar UI and added navlinks
    
    Added light mode and dark mode button

--- frontend/app/(components)/Navbar/index.tsx
+++ frontend/app/(components)/Navbar/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react'; 
-import {Search, Settings} from 'lucide-react'
+import {Menu, Moon, Search, Settings, Sun} from 'lucide-react'
 import Link from 'next/link';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
 const Navbar = () => {
-    
+    const dispatch = useAppDispatch(); 
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+    ) 
+    const isDarkMode = useAppSelector(
+        (state) => state.global.isDarkMode
+    )
 
     return (
         <div className='flex items-center justify-between bg-white px-4 py-3 dark:bg-black dark:px-4 dark:py-3 '>
+            {/* Open close */}
+            {!isSidebarCollapsed ? null : (
+                <button onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}>
+                    <Menu className='h-8 w-8 dark:text-white'/> 
+                </button>
+            ) }
+            
             {/* Search bar  */}
             <div className={`flex items-center gap-8`}> 
                 <div className={`relative flex h-min w-[200px]`}>
@@ -16,6 +31,15 @@ const Navbar = () => {
 
             {/* Icons */}
             <div className='flex items-center'>
+                {/* Light mode dark mode button */}
+                    <button onClick={() => dispatch(setIsDarkMode(!isDarkMode))} className={`${isDarkMode ? "h-min w-min rounded p-2 dark:hover:bg-gray-700" : `h-min w-min rounded p-2 dark:hover:bg-gray-200`}`}>
+                        {isDarkMode ? (
+                            <Sun className='h-6 w-6 cursor-pointer dark:text-white' /> 
+                        ) : (
+                            <Moon className='h-6 w-6 cursor-pointer dark:text-white' /> 
+                        )}
+                    </button>
+                {/* Settings Icon */}
                 <Link href="/settings" className='h-min w-min rounded p-2 hover:bg-gray-100'>
                     <Settings className='h-6 w-6 cursor-pointer dark:text-white'/>
                 </Link>
